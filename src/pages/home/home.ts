0710fc9b9a3cd151a8ddd7ceb5840bdf3a64b9e3
Anoop <AbdulRehman.Anoop@westlawjapan.com>
2018-02-27 18:24:18 +0900

    Adding parameters

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,11 +10,22 @@ import { Observable } from 'rxjs/Observable';
 export class HomePage {
 
   items: Observable<any[]>;
-  printTaken:boolean = true;
+  printTaken:boolean = false;
+  ZPrintTaken:boolean = false;
+  regClosed:boolean = false;
   invoiceDate:string;
   todaysSales:any = 0;
   startAmount:any = 0;
-  total:number = 0;
+  total:any = 0;
+  extraCash:any = 0;
+  totalCashTill:any = 0;
+  expenses:any = 0;
+  totalExpenses:any = 0;
+  creditCardPayments:any = 0;
+  mistakeAmount:any = 0;
+  ownerAmount:any = 0;
+  cashOwner:string = "";
+  mistake:string = "+";
   cashTill:{ 10000: number, 5000: number, 2000: number,1000: number, 500: number, 100: number,50: number, 10: number,5: number,1: number };
 
   constructor(public navCtrl: NavController, afDB: AngularFireDatabase, private alertCtrl: AlertController) {
@@ -26,6 +37,29 @@ export class HomePage {
     this.total = parseFloat(this.todaysSales) + parseFloat(this.startAmount);
   }
 
+  calculateTotalCashTill() {
+    this.totalCashTill = 10000*this.cashTill['10000'] +
+    5000*this.cashTill['5000'] +
+    2000*this.cashTill['2000'] +
+    1000*this.cashTill['1000'] +
+    500*this.cashTill['500'] +
+    100*this.cashTill['100'] +
+    50*this.cashTill['50'] +
+    10*this.cashTill['10'] +
+    5*this.cashTill['5'] +
+    1*this.cashTill['1'] +
+    parseFloat(this.extraCash);
+  }
+
+  checkAllExpenses() {
+    this.totalExpenses = parseFloat(this.totalCashTill) + parseFloat(this.expenses) + parseFloat(this.creditCardPayments) + parseFloat(this.ownerAmount);
+    if(this.mistake == "-")
+      this.totalExpenses = parseFloat(this.totalExpenses) - parseFloat(this.mistakeAmount);
+    else
+      this.totalExpenses = this.totalExpenses + parseFloat(this.mistakeAmount);
+    console.log(this.totalExpenses);
+  }
+
   addCashTill() {
     let alert = this.alertCtrl.create({
     title: 'Cash Till',
@@ -33,52 +67,62 @@ export class HomePage {
       {
         name: '10000',
         placeholder: '10000',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['10000'].toString():""
       },
       {
         name: '5000',
         placeholder: '5000',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['5000'].toString():""
       },
       {
         name: '2000',
         placeholder: '2000',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['2000'].toString():""
       },
       {
         name: '1000',
         placeholder: '1000',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['1000'].toString():""
       },
       {
         name: '500',
         placeholder: '500',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['500'].toString():""
       },
       {
         name: '100',
         placeholder: '100',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['100'].toString():""
       },
       {
         name: '50',
         placeholder: '50',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['50'].toString():""
       },
       {
         name: '10',
         placeholder: '10',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['10'].toString():""
       },
       {
         name: '5',
         placeholder: '5',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['5'].toString():""
       },
       {
         name: '1',
         placeholder: '1',
-        type: 'number'
+        type: 'number',
+        value:(this.cashTill)?this.cashTill['1'].toString():""
       }
     ],
     buttons: [
@@ -93,18 +137,19 @@ export class HomePage {
         text: 'Save',
         handler: data => {
           this.cashTill = {
-            10000:parseInt(data['10000']),
-            5000:parseInt(data['5000']),
-            2000:parseInt(data['2000']),
-            1000:parseInt(data['1000']),
-            500:parseInt(data['500']),
-            100:parseInt(data['100']),
-            50:parseInt(data['50']),
-            10:parseInt(data['10']),
-            5:parseInt(data['5']),
-            1:parseInt(data['1'])
+            10000:isNaN(parseInt(data['10000']))?0:parseInt(data['10000']),
+            5000:isNaN(parseInt(data['5000']))?0:parseInt(data['5000']),
+            2000:isNaN(parseInt(data['2000']))?0:parseInt(data['2000']),
+            1000:isNaN(parseInt(data['1000']))?0:parseInt(data['1000']),
+            500:isNaN(parseInt(data['500']))?0:parseInt(data['500']),
+            100:isNaN(parseInt(data['100']))?0:parseInt(data['100']),
+            50:isNaN(parseInt(data['50']))?0:parseInt(data['50']),
+            10:isNaN(parseInt(data['10']))?0:parseInt(data['10']),
+            5:isNaN(parseInt(data['5']))?0:parseInt(data['5']),
+            1:isNaN(parseInt(data['1']))?0:parseInt(data['1'])
           }
           console.log(this.cashTill);
+          this.calculateTotalCashTill();
         }
       }
     ]
@@ -155,4 +200,8 @@ export class HomePage {
   });
   alert.present();
 }*/
+
+  getStatus() {
+    return (this.totalExpenses==this.total || Math.abs(this.total-this.totalExpenses)<=500);
+  }
 }
